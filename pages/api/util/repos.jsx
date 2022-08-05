2c0b88207cfd108cc236831b50ff8495e0e13934
Swôth <56405862+Swothh@users.noreply.github.com>
2022-08-05 13:51:52 +0300

    Update repos.jsx

--- pages/api/util/repos.jsx
+++ pages/api/util/repos.jsx
@@ -4,7 +4,7 @@ export default async function Repos(req, res) {
     res.json(
         await (await fetch('https://api.github.com/users/nils-afk/repos', {
             headers: {
-                'Authorization': 'token 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'
+                'Authorization': 'token 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'
             } // get token: https://github.com/settings/tokens
         })).json()
     );
