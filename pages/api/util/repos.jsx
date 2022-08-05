9120d7539f111a59ab0c2e09871485a09ed0b8f6
Nils <97766170+Nils-afk@users.noreply.github.com>
2022-08-05 12:48:02 +0200

    Update repos.jsx

--- pages/api/util/repos.jsx
+++ pages/api/util/repos.jsx
@@ -4,7 +4,7 @@ export default async function Repos(req, res) {
     res.json(
         await (await fetch('https://api.github.com/users/nils-afk/repos', {
             headers: {
-                'Authorization': 'token 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'
+                'Authorization': 'token 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'
             } // get token: https://github.com/settings/tokens
         })).json()
     );
