d5765e0611bbd6c967fc952093e9010d7b8b6803
Nils <97766170+Nils-afk@users.noreply.github.com>
2022-08-05 11:55:21 +0200

    Update repos.jsx

--- pages/api/util/repos.jsx
+++ pages/api/util/repos.jsx
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-unfetch';
 
 export default async function Repos(req, res) {
     res.json(
-        await (await fetch('https://api.github.com/users/swothh/repos', {
+        await (await fetch('https://api.github.com/users/nils-afk/repos', {
             headers: {
                 'Authorization': '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'
             } // get token: https://github.com/settings/tokens
